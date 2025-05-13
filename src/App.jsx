4e6f54bd1177fd3e0b49cdd6b0f089850abfb9c3
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-13 11:42:00 +0200

    cards enfant

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import data from './data/data.json';
 import './App.css'
 import Personnes from './Components/Personnes/Personnes';
+import CardsEnfant from './Components/CardsEnfant/CardsEnfant';
+import Cards from './Components/CardsParent/cards';
 
 function App() {
   
 
   return (
     <>
-    <Personnes/>
+    <div className='divApp'>
+      <Personnes/>
+      <Cards/>
+    </div>
+    
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
     </>
   )
--- src/Components/CardsEnfant/CardsEnfant.jsx
+++ src/Components/CardsEnfant/CardsEnfant.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import './cardsenfant.css'
 
-export default function CardsEnfant() {
+export default function CardsEnfant({titre, heures, description, background, icon, image}) {
     return(
         <>
-        
+        <div className="divEnfant">
+            <div className={background} >
+            <img className={icon} src={image} alt="" />
+            </div>
+            <div className="divEnfantDiv2">
+                <div className="divEnfantDiv2Div">
+                    <h3 className="divEnfantH3">{titre}</h3>
+                    <span className="divEnfantspan">...</span>
+                </div>
+                <h1 className="divEnfantH1"> {heures} </h1>
+                <p className="divEnfantP">{description}</p>
+            </div>
+        </div>
         </>
     )
 }
