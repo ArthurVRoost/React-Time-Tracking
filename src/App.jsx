d6b6ff70af8bf9c79020f674098482a8de8b092d
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-13 15:07:00 +0200

    debut ligtmode

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import data from './data/data.json';
 import './App.css'
 import Personnes from './Components/Personnes/Personnes';
 import Cards from './Components/CardsParent/cards';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [activePeriod, setActivePeriod] = useState("weekly")
-
+  const [lightMode, setLightMode] = useState(false)
+  const toggleTheme = () =>{
+    setLightMode(prev => !prev)
+  }
   return (
     <>
-    <div className='divApp'>
+    <FontAwesomeIcon className='fa-sun' icon={faSun} onClick={toggleTheme} />
+    <div className={`divApp ${lightMode ? 'light-mode' : ''}`}>
       <Personnes activePeriod={activePeriod} setActivePeriod={setActivePeriod}/>
       <Cards activePeriod={activePeriod} data={data}/>
     </div>
