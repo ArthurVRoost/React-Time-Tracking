15e44b1ccc8fdf1861cc24ff8ecf74743c6fbe1a
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-13 10:06:34 +0200

    component personnes fini css

--- src/Components/Personnes/Personnes.jsx
+++ src/Components/Personnes/Personnes.jsx
@@ -5,14 +5,14 @@ export default function Personnes() {
         <>
         <div className='divPersonnes'>
             <div className='divPersonnesDiv1'>
-                <img src="../../src/assets/images/image-jeremy.png" alt="" />
-                <h3>Report for</h3>
-                <h1>Jeremy <br />Robson</h1>
+                <img className='divPersonnesImg' src="../../src/assets/images/image-jeremy.png" alt="image de jeremey ce beau gosse afou" />
+                <p className='divPersonnesP'>Report for</p>
+                <h1 className='divPersonnesH1'>Jeremy <br />Robson</h1>
             </div>
             <div className='divPersonnesDiv2'>
-                <h2>Daily</h2>
-                <h2>Weekly</h2>
-                <h2>Monthly</h2>
+                <h2 className='divPersonnesH2 topH2'>Daily</h2>
+                <h2 className='divPersonnesH2'>Weekly</h2>
+                <h2 className='divPersonnesH2'>Monthly</h2>
             </div>
         </div>
         </>
