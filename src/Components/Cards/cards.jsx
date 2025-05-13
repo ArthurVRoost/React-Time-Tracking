1b388296dd116c2b02e315e10eec8b2c4f35f25a
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-13 09:33:34 +0200

    creation components

---
+++ src/Components/Cards/cards.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import './cards.css'
+export default function Cards(params) {
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/Components/Personnes/Personnes.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './personnes.css'
+export default function Personnes() {
+    return(
+        <>
+        
+        </>
+    )
+}
